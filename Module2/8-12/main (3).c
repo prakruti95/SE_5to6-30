45ff4d8e54743b83177f6af914e4f929e2a50be2
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-12-08 18:19:52 +0530

    commit

---
+++ Module2/8-12/main (3).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   
+    int a = 4;
+    int b = 2;
+    int c = 4;
+    
+    printf("\n %d",a<b);
+    printf("\n %d",a>b);
+    printf("\n %d",a<=c);
+    printf("\n %d",a>=b);
+    printf("\n %d",a==b);
+    printf("\n %d",a!=b);
+    
+    getch();
+}
