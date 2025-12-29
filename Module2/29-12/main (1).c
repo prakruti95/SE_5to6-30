e1dd8d73744bde3b293305a5a022a2349209e284
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-12-29 18:30:17 +0530

    commit

---
+++ Module2/29-12/main (1).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() 
+{
+   int a=5,b=10;
+   
+    a=a+b;//5+10// 15
+    b=a-b;//15-10// 5
+    a=a-b;//15-5// 10    
+   
+   
+   printf("\n %d",a);//10
+   printf("\n %d",b);//5
+
+    return 0;
+}
---
+++ Module2/29-12/main.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int a=5,b=10,c;
+   
+    c = a;
+    a = b;
+    b = c;
+   
+   printf("\n %d",a);
+   printf("\n %d",b);
+
+    return 0;
+}
