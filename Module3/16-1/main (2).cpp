3803e020f3e27a6baa45addcbbe7e192ba4de9dd
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2026-01-17 09:53:42 +0530

    commit

---
+++ Module3/16-1/main (2).cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+
+int main()
+{
+    int num;
+    std::cout<<"Enter Your Number";
+    std::cin>>num;
+    std::cout<<"Your Number is : "<<num;
+    return 0;
+}
---
+++ Module3/16-1/main (4).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     int num;
+     
+     cout<<"Enter Your Number";
+     cin>>num;
+     
+    if(num%2==0)
+    {
+        cout<<"Even";
+    }
+    else
+    {
+        cout<<"Odd";
+    }
+
+    return 0;
+}
