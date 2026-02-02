87add41b259df14cd719b02a8b6f44a43b071f29
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2026-02-02 18:21:10 +0530

    commit

---
+++ Module3/2-2/main (11).cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+ class Shape    
+{    
+    public:   
+    virtual void draw()=0;
+   
+};  
+
+ class Rectangle : public Shape
+{    
+    public: 
+     void draw()    
+    {    
+        cout <<"drawing rectangle..." <<endl;    
+    }   
+   
+};  
+
+
+ class Circle : public Shape
+{    
+    public:   
+    void draw()    
+    {    
+        cout <<"drawing circle..." <<endl;    
+    }   
+};  
+
+int main()
+{
+       Rectangle rec;
+       Circle cir;
+       
+       rec.draw();
+       cir.draw();
+
+    return 0;
+}
